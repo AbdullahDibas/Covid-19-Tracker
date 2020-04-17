bd63a8814b85c52b1a951e87d6f14638743db183
AbdullahDibas <AbdallahD@JValley.local>
2020-04-17 19:12:00 +0300

    Change the listening port for production

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
